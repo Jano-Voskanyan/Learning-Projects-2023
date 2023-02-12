5caecc3744c06420310554ac51f1df2e704d3a80
Janibek <jano.vr@mail.ru>
2023-02-12 19:15:33 +0400

    Auto Layout

---
+++ Angela-TipsyApp-Storyboard/Tipsy/Tipsy/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecalculateViewController.swift
+//  Tipsy
+//
+//  Created by Janibek Voskanyan on 12.02.23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

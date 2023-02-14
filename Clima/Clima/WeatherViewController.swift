f84d977c2a13100ecef01d8587be78d1e28aecaf
Janibek <jano.vr@mail.ru>
2023-02-14 11:40:25 +0400

    add Design for start add design without constraints for iPhone 11

---
+++ Clima/Clima/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Janibek Voskanyan on 14.02.23.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
